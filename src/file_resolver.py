e1d6d4f33aa11351905c8ca8bd6a301ed4d664f3
umi <asiessoyeltier1@gmail.com>
2024-06-06 22:08:50 -0500

    [dev] FileResolver read inner data
    The file resolver have to make a more complex opreation to return the data
    line by line, in this commit is registered a failed first attempt, clearly
    marked above the line.

--- src/file_resolver.py
+++ src/file_resolver.py
@@ -9,38 +9,35 @@ class FileResolver:
     absolute_path
     """
 
-    def __init__(self, name: str, prefix = "./cache.kaif"):
+    def __init__(self, name: str, encoding = "utf-8" , prefix = "./cache.kaif"):
         """
         {prefix} Is a relative path by default, must specify the absolute location overriding the prefix param
         {name} Name of the file or folder, with only allowed symbols by the OS
         """
 
         # Sanitize input
-        result = self.sanitize(path=name)
+        result = self._sanitize(path=name)
 
-        # Resolve folder: prefix
+        # DEBUG Resolve folder: prefix
         print(f"The prefix is: {prefix}")
 
-        self.resolve_folder(folder_name=prefix)
+        self._resolve_folder(folder_name=prefix)
 
         # Create the file
-        full_path: str = f"{prefix}/{result}"
+        full_path:str = f"{prefix}/{result}"
 
-        self.absolute_path = full_path
+        self._resolve_file(file_name=full_path)
+
+        # Save different route versions
+        self.absolute_path:str = full_path
+
+        self.relative_path:str = result
         
 
-    def resolve_folder(self,folder_name: str) -> str:
-
-        Path(folder_name).mkdir(parents=True, exist_ok=True)
-
-        return folder_name
-
-    def read_inner_file(self):
-        pass
-
-    def sanitize(self, path:str) -> str:
+    def _sanitize(self, path:str) -> str:
         """
         Cleans the path to avoid bugs
+        {path}
         """
         # Separete the extension
         [neutral_name, extension] = os.path.splitext(path)
@@ -50,3 +47,31 @@ class FileResolver:
         #index = re.search("", neutral_name).start()
 
         return f"{neutral_name}{extension}"
+    def _resolve_folder(self,folder_name: str) -> str:
+        """
+        Resolves to create the folder
+        {folder_name}
+        """
+
+        Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+        return folder_name
+
+    def _resolve_file(self, file_name: str) -> None:
+        """
+        Resolves the creation of the file
+        {file_name} 
+        """
+        # TEST => DO NOT WORK
+        with open(file_name, "a", encoding="utf-8") as file:
+            return
+
+    def read_inner_file(self, encoding = "utf-8"):
+        """
+        Return the data from the file dispatched and returns its value
+        {encoding} The default value for encoding is utf-8
+        """
+
+        # Gets the content and return it
+        with open(self.absolute_path, "r", encoding = encoding) as data:
+            return data
